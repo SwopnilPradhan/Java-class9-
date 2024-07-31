5e300ab01f3cc9147bea48292de273d70a458cae
Pradhanians <163707646+Pradhanians@users.noreply.github.com>
2024-07-31 15:42:03 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class rev{
+public class reverse{
   public static void main(String args[]){
     String x = "Deepak";
     int length = x.length();
     String rev = "";
-    for(int i = length-1;i>=0;i++){
+    for(int i = length-1;i>=0;i--){
       rev = rev + charAt(i);
     }
     System.out.println(rev);
