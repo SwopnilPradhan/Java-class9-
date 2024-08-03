c8182c03b41931c42cc41c12c9174620a4a79d20
Swapnil Pradhan <163707646+Pradhanians@users.noreply.github.com>
2024-08-03 11:52:14 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Vowel{
+  public static void main(String args[]){
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter letter");
+    char ch = sc.next().charAt(0);
+    switch(ch){
+      case 'A':
+      case 'a':
+        System.out.println("vowel");
+      case 'E':
+      case 'e':
+        System.out.println("vowel"); 
+      case 'I':
+      case 'i':
+        System.out.println("vowel");
+      case 'O':
+      case 'o':
+        System.out.println("vowel");
+      case 'U':
+      case 'u':
+        System.out.println("vowel");
+      default:
+        System.out.println("not a vowel");
